3d65ee4a6e6a6025469dc268f213fe4f43fdccba
meonoeek@gmail.com <meonoeek@gmail.com>
2020-09-04 21:26:54 +0300

    2. Без переопределения equals и hashCode[1005#334449]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,15 +1,45 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class User {
-   private String name;
-   private int children;
-   private Calendar birthday;
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public static void main(String[] args) {
+        User ivan = new User("Ivan", 4, Calendar.getInstance());
+        User petr = new User("Petr", 2, Calendar.getInstance());
+        Map<User, Object> users = new HashMap<>();
+        users.put(ivan, 1);
+        users.put(petr, 2);
+        Iterator<Map.Entry<User, Object>> iterator = users.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<User, Object> pair = iterator.next();
+            User key = pair.getKey();
+            Object value = pair.getValue();
+            System.out.println(value + ":name-" + key.name + " children-" + key.children);
+        }
     }
 }
